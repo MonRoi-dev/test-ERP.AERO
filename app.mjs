7d5c3939f31881eeab88f9375a8ea0903657ee9f
David Arutiunyan <monroi.dev@gmail.com>
2024-04-14 15:33:57 +0300

    added cors support and secure file routes

--- app.mjs
+++ app.mjs
@@ -1,16 +1,19 @@
 import express from 'express';
 import authRouter from './routes/authRoute.mjs';
 import fileRouter from './routes/fileRoute.mjs';
+import { checkToken } from './middlewares/checkToken.mjs';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.REFRESH_SECRET));
+app.use(cors());
 
 app.use(authRouter);
-app.use('/file', fileRouter);
+app.use('/file', checkToken, fileRouter);
 
 const port = process.env.PORT || 5000;
 
